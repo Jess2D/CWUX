d66304eb21b05013db6c852cb7625d48bf352da5
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-04-07 21:18:58 +1200

    Add List of products

--- template/script.js
+++ template/script.js
@@ -1,14 +1,16 @@
-const MyStepper = document.getElementById("my-input");
+//const MyStepper = document.getElementById("my-input");
+function getIdFromStepper(id) {
+  return id;
+}
 
-function Stepper(btn){
-    let min = MyStepper.getAttribute("min");
-    let max = MyStepper.getAttribute("max");
-    let val = MyStepper.getAttribute("value");
-    console.log(val)
-    let newValue = (btn == "increment") ? ++val  : --val;
-    if(newValue >= min && newValue <= max ) {
-        MyStepper.setAttribute("value", newValue)
-    }
- 
-    
+function Stepper(btn, input) {
+  const MyStepper = document.getElementById(input);
+  let min = MyStepper.getAttribute("min");
+  let max = MyStepper.getAttribute("max");
+  let val = MyStepper.getAttribute("value");
+  console.log(val);
+  let newValue = btn == "increment" ? ++val : --val;
+  if (newValue >= min && newValue <= max) {
+    MyStepper.setAttribute("value", newValue);
+  }
 }

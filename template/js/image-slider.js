d0a94a5a5bfdca8847c37f3d871816fb334aba4f
Maira <mairalds@gmail.com>
2022-04-08 18:49:25 +1200

    Eu terminei a Home page

---
+++ template/js/image-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.deals').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+    });
+  });

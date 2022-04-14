ca27829cffdc8298ae29ca4e0e8b83073e5de281
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-04-14 18:17:36 +1200

    fix: form inside div w3c standantrs

--- template/js/teas.js
+++
@@ -1,9 +0,0 @@
-const radios = document.getElementsByName("milk");
-
-for (var i = 0; i < radios.length; i++) {
-  if (radios[i].checked) {
-    document.write(radios[i], "radio button is selected");
-    console.log(radios[i].value);
-    break;
-  }
-}
